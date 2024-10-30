1ad49b160d5beb23e83fc0f8a2f9f630e3da1803
AleksandarJanev <janevaleksandar7@hotmail.com>
2024-10-30 13:39:52 +0100

    Initial commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import re
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def fetch_issuer_names(url):
     """Fetch issuer names from the given URL and filter those without numbers in their codes."""
@@ -33,6 +34,29 @@ def save_to_csv(issuer_names, filename='filtered_issuers.csv'):
         writer.writerow(['Issuer Name'])  # Only the name
         writer.writerows([[name] for name in issuer_names])  # Write each name as a row
     print(f"Filtered issuer names saved to {filename}")
+
+def listanje_na_url(sifri):
+    for name in sifri:
+        url="https://www.mse.mk/mk/stats/symbolhistory/"+name
+        print(url)
+
+
+def print_first_two_columns(url, start_date, end_date):
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    from_date = driver.find_element(By.ID, "FromDate")
+    from_date.clear()
+    from_date.send_keys(start_date)
+
+    to_date = driver.find_element(By.ID, "ToDate")
+    to_date.clear()
+    to_date.send_keys(end_date)
+
+    search_button = driver.find_element(By.ID, "submitButton")  # прилагоди го ID ако е различен
+    search_button.click()
+    driver.quit()
+
 
 
 def main():
